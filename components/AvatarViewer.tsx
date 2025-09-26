9e068c412ef4ce657413fef37fe400151cd6c9c8
Erfan Ansari <dev.erfanansari@gmail.com>
2025-09-27 03:21:58 +0330

    chore: remove pan feature

--- components/AvatarViewer.tsx
+++ components/AvatarViewer.tsx
@@ -380,10 +380,6 @@ export default function AvatarViewer({}: AvatarViewerProps) {
                       <div className="w-2 h-2 bg-green-400 rounded-full"></div>
                       <span>Scroll to zoom</span>
                     </div>
-                    <div className="flex items-center gap-2">
-                      <div className="w-2 h-2 bg-purple-400 rounded-full"></div>
-                      <span>Right-click to pan</span>
-                    </div>
                   </div>
                 </div>
               </div>
